a73f3801bd04e42db841c9e04ff39d88d5b17644
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-18 19:49:50 +0700

    VueJs Using Conditionals and Rendering Lists

---
+++ S2-ConditionalsAndRendering/app.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+    show: false,
+    ingredients: ['meat', 'fruits', 'cookies'],
+    people: [
+      {
+        name: 'Clark',
+        age: 39,
+        color: ['red', 'blue']
+      },
+      {
+        name: 'bruce',
+        age: 37,
+        color: ['black', 'gray']
+      }
+    ]
+  }
+});
