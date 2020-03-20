d19e65c6c95e9f859fb3331287518f01436acf3a
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-20 19:34:31 +0700

    Handling User Input with Forms

---
+++ S10-UserInputForms/Exercise/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="container">
+  <form>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <!-- Exercise 1 -->
+        <!-- Create a Signup Form where you retrieve the following Information -->
+        <!-- Full Name (First Name + Last Name) -->
+        <!-- Mail -->
+        <!-- Password -->
+        <!-- Store Data? Yes/No -->
+
+        <!-- Exercise 2 -->
+        <!-- Only display the Form if it has NOT been submitted -->
+        <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+        <!-- Exercise 3 -->
+        <!-- Edit the Example from above and create a custom "Full Name" Control -->
+        <!-- which still holds the First Name and Last Name Input Field -->
+      </div>
+    </div>
+  </form>
+  <hr>
+  <div class="row" v-if="isSubmitted">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4>Your Data</h4>
+        </div>
+        <div class="panel-body">
+          <p>Full Name: {{ userData.firstName }} {{ userData.lastName }}</p>
+          <p>Mail: {{ userData.email }}</p>
+          <p>Password: {{ userData.password }}</p>
+          <p>Store in Database?: {{ userData.verification }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row" v-else>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h3>Signup</h3>
+        </div>
+        <div class="panel-body">
+          <signup-app :userData="userData" :isSubmitted="isSubmitted" @submitted="isSubmitted = $event" @user="userData = $event"></signup-app>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+  import Signup from './assets/components/Signup.vue'
+
+  export default {
+    data() {
+      return {
+        isSubmitted: false,
+        userData: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+          verification: 'no'
+        }
+      }
+    },
+    components: {
+      'signup-app': Signup
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ S10-UserInputForms/Exercise/src/assets/components/Signup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form>
+      <div class="form-group">
+        <label>First Name</label>
+        <input class="form-control" type="text" v-model="userData.firstName">
+      </div>
+
+      <div class="form-group">
+        <label>Last Name</label>
+        <input class="form-control" type="text" v-model="userData.lastName">
+      </div>
+
+      <div class="form-group">
+        <label>Email</label>
+        <input class="form-control" type="text" v-model="userData.email">
+      </div>
+
+      <div class="form-group">
+        <label>Password</label>
+        <input class="form-control" type="password" v-model="userData.password">
+      </div>
+
+      <div class="form-group">
+        <label for="yes">
+          <input type="radio" value="Yes" v-model="userData.verification"> Yes
+        </label>
+        <label for="no">
+          <input type="radio" value="No" v-model="userData.verification"> No
+        </label>
+      </div>
+
+      <div class="form-group">
+        <button class="btn btn-primary" type="button" name="button" @click="submitUser">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['userData', 'isSubmitted'],
+    methods: {
+      submitUser() {
+        this.isSubmitted = true;
+        this.$emit('submitted', this.isSubmitted);
+        this.$emit('user', this.userData);
+      },
+      verify(condition) {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
