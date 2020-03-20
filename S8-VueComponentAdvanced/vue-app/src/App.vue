b285fd2a4bd1f1784010548bf7ae222f59c55651
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-20 07:29:19 +0700

    VueJs Advanced Component Usage

---
+++ S8-VueComponentAdvanced/vue-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <button type="button" @click="selectedComponent = 'app-quote'">Quote</button>
+        <button type="button" @click="selectedComponent = 'app-author'">Author</button>
+        <button type="button" @click="selectedComponent = 'app-new'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>Default Content</p>
+          </component>
+        </keep-alive>
+
+        <!-- <app-quote>
+          <h1 slot="title">{{ title }}</h1>
+          <p>A Wonderful Quote</p>
+        </app-quote> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue'
+  import Author from './components/Author.vue'
+  import New from './components/New.vue'
+
+  export default {
+    data: function() {
+      return {
+        title: 'The Quote',
+        selectedComponent: 'app-quote'
+      }
+    },
+    components: {
+      'app-quote': Quote,
+      'app-author': Author,
+      'app-new': New
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ S8-VueComponentAdvanced/vue-app/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button type="button" @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        counter: 0
+      }
+    },
+    destroyed() {
+      console.log('destroyed');
+    },
+    activated() {
+      console.log('activated');
+    },
+    deactivated() {
+      console.log('dectivated');
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
